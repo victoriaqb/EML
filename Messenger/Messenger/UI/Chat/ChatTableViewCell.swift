1c39686f2daf879922d7ec79951a49a37b4b06a0
Victoria Kashlina <v.o.kashlina@gmail.com>
2018-05-06 18:34:42 +0300

    chatTableCell

---
+++ Messenger/Messenger/UI/Chat/ChatTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  ChatTableViewCell.swift
+//  Messenger
+//
+//  Created by Victoria Kashlina on 06/05/2018.
+//  Copyright © 2018 None. All rights reserved.
+//
+
+import UIKit
+
+public class ChatTableViewCell : UITableViewCell {
+    
+    @IBOutlet private weak var titleLabel: UILabel?
+    @IBOutlet private weak var authorLabel: UILabel?
+    @IBOutlet private weak var messageLabel: UILabel?
+    
+    public override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.titleLabel = UILabel()
+        self.authorLabel = UILabel()
+        self.messageLabel = UILabel()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    public override func prepareForReuse() {
+        self.titleLabel?.text = nil
+        self.authorLabel?.text = nil
+        self.messageLabel?.text = nil
+    }
+}
+
+
